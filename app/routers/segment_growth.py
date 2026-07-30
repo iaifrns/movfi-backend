df77a383f95b5d9390a8bb9a70cf3fb550f7f1b9
iaifrns <nsinifranc@gmail.com>
2026-07-30 15:41:55 +0100

    finished with the end point of general joint point

--- app/routers/segment_growth.py
+++ app/routers/segment_growth.py
@@ -51,3 +51,38 @@ def get_all_frames_segment(fish_id: str, db: Session = Depends(get_db)):
         print(e)
         print('*'*80)
         raise HTTPException(status_code=500, detail=f"An error occured {e}")
+
+@router.get('/get_a_segment/{fish_id}', response_model=list)
+def get_segment(fish_id: str, db: Session = Depends(get_db)):
+    try:
+
+        fish = db.query(model.FishData).filter(model.FishData.id == fish_id).first()
+
+        if not fish:
+            raise HTTPException(status_code=404, detail=f"No fish was found with this id")
+        
+        fileData = db.query(model.FileData).filter(model.FileData.fish_id == fish.id).first()
+
+        data = []
+
+        for d in fileData.data:
+            data.append(list(d.values()))
+
+        xs = list(range(0,len(data[0]),2))
+        ys = list(range(1, len(data[0]),2))
+
+        joint_positions, total_evaluations, total_datapoints = segment_growing(
+            data=np.array(data),
+            xs=np.array(xs),
+            ys=np.array(ys),
+            num_rows=len(fileData.data),
+            num_cols=len(fileData.data[0].keys()),
+            thresh=0.001
+        )
+
+        return joint_positions.tolist()
+    except Exception as e:
+        print(e)
+        print('*'*80)
+        raise HTTPException(status_code=500, detail=f"An error occured {e}")
+     
